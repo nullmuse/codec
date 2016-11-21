b1251b6db3b740f44dc546f2e7d3d5df75acb12e
nullmuse <dylan.liebgott@gmail.com>
2016-11-21 15:37:47 +0000

    add pcap file header writing

--- dragoon.c
+++ dragoon.c
@@ -11,15 +11,25 @@ int holder;
 short holder_s; 
 char *pcap_header = calloc(PCAP_SIZE, sizeof(char)); 
 
+struct pcap_header_f file_header; 
+
+file_header.major_version = MJ_VER;
+file_header.minor_version = MN_VER; 
+file_header.gmt_offset = 0; 
+file_header.acc_delta = 0; 
+file_header.maxlen = 65536; 
+file_header.link_type = 1; 
+ 
+
 memcpy(pcap_header,psychic_header,HEADER_SIZE); 
 
 if(!strncmp(pcap_header,psychic_header,HEADER_SIZE)) {
 printf("Worked\n"); 
 }
 
-memcpy((pcap_header + OFF_MJVER),&MJ_VER,sizeof(short)); 
+memcpy((pcap_header + OFF_MJVER),&file_header,sizeof(struct pcap_header_f)); 
 
-memcpy((pcap_header + OFF_MNVER),&MN_VER,sizeof(short));
+//memcpy((pcap_header + OFF_MNVER),&MN_VER,sizeof(short));
 
 
 return pcap_header; 
