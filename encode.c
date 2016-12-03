1a1c0b8e3a8f1337cd8ee782572baa826f16608c
nullmuse <dylan.liebgott@gmail.com>
2016-12-03 08:12:51 -0500

    Make encode style compliant

--- encode.c
+++ encode.c
@@ -5,7 +5,11 @@
  #include "codelib.h"
  #include "pcap.h"
  
- int main(int argc, char *argv[]) {
+int
+main(
+int argc,
+char *argv[])
+{
     char *rFile = NULL;
     char *rTok = NULL;
     int zergLen; 
@@ -15,43 +19,45 @@
     char *payload; 
     char *totalData;
     char *pcapHeader; 
-    if(argc != 2) { 
+    if(argc != 2)
+    {
        printf("%s descriptionFile\n",argv[0]); 
        return 1;
-       }
- initPcap("encoded.pcap");
- rFile = readFile(argv[1],0);
- rTok = strtok(rFile, "\t");
- while(rTok != NULL) {
-    if(!strncmp(rTok,"\n\n",2))
-    break;
-    if(!strcmp(rTok,"\n"))
-    break;
-    if(petalHandler(rTok,strlen(rTok))) {
-    free(rFile);
-    return 1;
     }
-    pcapHeader = pcapBuildInit();
-    pcapBuildHeader(pcapHeader,0); 
-    pcapBuildEth(pcapHeader); 
-    pcapBuildIph(pcapHeader);
-    pcapBuildUdp(pcapHeader); 
-    zHeader = transmuteInput(rTok);  
-    payload = zergTypeEncoder(zHeader,rTok, &zergLen);
-    totalData = calloc(PCAP_SIZE + zergLen,sizeof(char)); 
-    memcpy(totalData,pcapHeader,PCAP_SIZE); 
-    memcpy(totalData + PCAP_SIZE,payload,zergLen);
-    sizeFixups(totalData);
-    writePcap((totalData + headerAdd),pSize + zergLen,"encoded.pcap");
-    free(totalData); 
-    free(pcapHeader);
-    free(payload); 
-    free(zHeader); 
-    rTok = strtok(NULL, "\t");
-    pSize = PCAP_SIZE - 24;
-    headerAdd = 24;
+    initPcap("encoded.pcap");
+    rFile = readFile(argv[1],0);
+    rTok = strtok(rFile, "\t");
+    while(rTok != NULL)
+    {
+        if(!strncmp(rTok,"\n\n",2))
+            break;
+        if(!strcmp(rTok,"\n"))
+            break;
+        if(petalHandler(rTok,strlen(rTok)))
+        {
+            free(rFile);
+            return 1;
+        }
+        pcapHeader = pcapBuildInit();
+        pcapBuildHeader(pcapHeader,0); 
+        pcapBuildEth(pcapHeader); 
+        pcapBuildIph(pcapHeader);
+        pcapBuildUdp(pcapHeader); 
+        zHeader = transmuteInput(rTok);  
+        payload = zergTypeEncoder(zHeader,rTok, &zergLen);
+        totalData = calloc(PCAP_SIZE + zergLen,sizeof(char)); 
+        memcpy(totalData,pcapHeader,PCAP_SIZE); 
+        memcpy(totalData + PCAP_SIZE,payload,zergLen);
+        sizeFixups(totalData);
+        writePcap((totalData + headerAdd),pSize + zergLen,"encoded.pcap");
+        free(totalData); 
+        free(pcapHeader);
+        free(payload); 
+        free(zHeader); 
+        rTok = strtok(NULL, "\t");
+        pSize = PCAP_SIZE - 24;
+        headerAdd = 24;
     }
- free(rFile); 
- return 0; 
- 
- }
+    free(rFile); 
+    return 0; 
+}
