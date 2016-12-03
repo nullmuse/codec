0d23dad48d7df908677a1b844b3d8560ce865419
nullmuse <dylan.liebgott@gmail.com>
2016-12-03 08:23:58 -0500

    Make cfile style compliant

--- cfile.c
+++ cfile.c
@@ -3,108 +3,112 @@
  #include <sys/stat.h>
  #include <string.h>
  #include "pcap.h"
- 
- 
- int getFileSize(char *fName) {
+
+int
+getFileSize(
+char *fName)
+{
     struct stat fileInfo;
     int ret;
-    if(stat(fName,&fileInfo) == -1) {
-              return -1;
-       }
- ret = fileInfo.st_size;
- return ret;
+    if(stat(fName,&fileInfo) == -1)
+    {
+        return -1;
+    }
+    ret = fileInfo.st_size;
+    return ret;
  
- }
- 
- 
- 
- int validateFile(FILE *fp) {
-    
+}
+
+int
+validateFile(FILE *fp)
+{
     int ret = OP_FAIL; 
     char *pCheck; 
-    
-    
     pCheck = calloc(HEADER_SIZE + 1,sizeof(char)); 
-    
     fread(pCheck,sizeof(char),HEADER_SIZE,fp);
-    
-    if(!strncmp(pCheck,psychicHeader,HEADER_SIZE)) { 
-       ret = OP_SUCCESS; 
-       }
- 
- free(pCheck); 
- return ret; 
- 
- } 
- 
- 
- char *readFile(char *fName, int isPcap) { 
-    
+    if(!strncmp(pCheck,psychicHeader,HEADER_SIZE))
+    {
+        ret = OP_SUCCESS; 
+    }
+    free(pCheck); 
+    return ret; 
+}
+
+char *
+readFile(
+char *fName,
+int isPcap)
+{
     int fSize = 0; 
     char *retData = NULL;
     FILE *fp = NULL; 
-    
     fSize = getFileSize(fName);
-    if(fSize <= 0) {
-             perror("FATAL: is empty file");
-             goto RETURN;
-             }
- if ((fp = fopen(fName, "rb")) == NULL) {
-         perror("FATAL: cannot open file");
+    if(fSize <= 0)
+    {
+        perror("FATAL: is empty file");
+        goto RETURN;
+    }
+    if ((fp = fopen(fName, "rb")) == NULL)
+    {
+        perror("FATAL: cannot open file");
          goto RETURN;
     }
- 
- if(isPcap) { 
-    if(validateFile(fp) == OP_FAIL) {
-       perror("FATAL: Not a valid pcap file"); 
-       goto RETURN; 
-       } 
- rewind(fp); 
- } 
- retData = calloc(fSize + 1, sizeof(char)); 
- 
- fread(retData,sizeof(char),fSize,fp); 
- 
- 
- RETURN:
- if(fp)
- fclose(fp); 
- return retData; 
- }
- 
- int writePcap(char *pcapData,int pcapSize,char *fileName) {
-    
+
+    if(isPcap)
+    {
+        if(validateFile(fp) == OP_FAIL)
+        {
+            perror("FATAL: Not a valid pcap file"); 
+            goto RETURN; 
+        } 
+        rewind(fp); 
+    }
+    retData = calloc(fSize + 1, sizeof(char)); 
+    fread(retData,sizeof(char),fSize,fp); 
+
+RETURN:
+    if(fp)
+    fclose(fp); 
+    return retData; 
+}
+
+int
+writePcap(
+char *pcapData,
+int pcapSize,
+char *fileName)
+{
     int retData = 0;
     FILE *fp;
-    if (getFileSize(fileName) == -1) { 
-       
-       if ((fp = fopen(fileName, "wb+")) == NULL) {
-               perror("FATAL: cannot open file");
-               goto RETURN;
-          }
- }
- else { 
-    if ((fp = fopen(fileName, "ab+")) == NULL) {
+    if (getFileSize(fileName) == -1)
+    {
+        if ((fp = fopen(fileName, "wb+")) == NULL)
+        {
             perror("FATAL: cannot open file");
             goto RETURN;
-       }
- }
- 
- fwrite(pcapData, sizeof(char), pcapSize,fp);
- fclose(fp);
- 
- 
+        }
+    }
+    else
+    {
+        if ((fp = fopen(fileName, "ab+")) == NULL)
+        {
+            perror("FATAL: cannot open file");
+            goto RETURN;
+        }
+    }
+    fwrite(pcapData, sizeof(char), pcapSize,fp);
+    fclose(fp);
  RETURN:
  return retData;
- 
  }
- 
- void initPcap(char *fileName) { 
-    if (getFileSize(fileName)) {
-       remove(fileName); 
-       }
- }
- 
- 
- 
- 
+
+void
+initPcap(
+char *fileName)
+{
+    if (getFileSize(fileName))
+    {
+        remove(fileName); 
+    }
+}
+
